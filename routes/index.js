e1d07d4c4496de62774472da6a3a276f5b3cd005
Aioncheg <sch1081013@gmail.com>
2015-04-09 23:17:03 +0500

    complete the task

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,51 @@ var appdata = require('../data.json');
 /* GET home page. */
 router.get('/', function(req, res) {
 	var myArtwork = [];
+	var myArtists = [];
+	myArtists = appdata.speakers;
+
 	appdata.speakers.forEach(function(item) {
 		myArtwork = myArtwork.concat(item.artwork);
 	});
   res.render('index', { 
-  	title: 'Express',
-  	artwork: myArtwork
+  	title: 'Home',
+  	artwork: myArtwork,
+		artists: myArtists,
+		page: 'home'
   });
 });
 
+router.get('/speakers', function(req, res) {
+	var myArtwork = [];
+	var myArtists = [];
+	myArtists = appdata.speakers;
+
+	appdata.speakers.forEach(function(item) {
+		myArtwork = myArtwork.concat(item.artwork);
+	});
+	res.render('speakers', {
+		title: 'Speakers',
+		artwork: myArtwork,
+		artists: myArtists,
+		page: 'artistList'
+	});
+});
+
+router.get('/speakers/:speakerid', function(req, res) {
+	var myArtwork = [];
+	var myArtists = [];
+
+	appdata.speakers.forEach(function(item) {
+		if (item.shortname === req.params.speakerid) {
+			myArtists.push(item);
+			myArtwork = myArtwork.concat(item.artwork);
+		}
+	});
+	res.render('speakers', {
+		title: 'Speakers',
+		artwork: myArtwork,
+		artists: myArtists,
+		page: 'artistDetail'
+	});
+});
 module.exports = router;
